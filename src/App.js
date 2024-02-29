0238de892f5b06b0d177485af059ca5b1f6c8ffa
Eddy Viet <eddyviettc@gmail.com>
2024-02-29 16:08:38 +0700

    create top nav and css nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './views/Nav.js'
 
 function App() {
+  let name = 'Reverie'
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Reverie from Eddy</h1>
+        <h1>Hello {name} from Eddy</h1>
       </header>
     </div>
   );
